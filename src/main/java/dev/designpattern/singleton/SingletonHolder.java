cd6bfe31ac97529e99ec93cd9c8070ff809c1154
UMESH TOKE <umeshtoke09@gmail.com>
2023-11-04 23:43:32 +0530

    updates Bill Pugh singleton

--- src/main/java/dev/designpattern/singleton/SingletonHolder.java
+++ src/main/java/dev/designpattern/singleton/SingletonHolder.java
@@ -1,3 +1,10 @@
+/** 
+Bill Pugh Implementation of singleton
+The private inner static class that contains the instance of the singleton class. 
+When the SingletonHolder class is loaded, SingletonInstanceHolder class is not loaded into memory and only when someone calls the instance() method, 
+this class gets loaded and creates the SingletonHolder class instance. 
+This is the most widely used approach for the singleton class as it doesn’t require synchronization
+**/
 package dev.designpattern.singleton;
 
 public class SingletonHolder {
