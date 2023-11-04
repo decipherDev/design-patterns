c826a7b91a5531e9b5667b6cf94853c2e35a3623
decipherDev <umesh.toke09@gmail.com>
2023-11-05 00:19:02 +0530

    updates singleton enum

--- src/main/java/dev/designpattern/singleton/SingletonEnum.java
+++ src/main/java/dev/designpattern/singleton/SingletonEnum.java
@@ -1,6 +1,7 @@
 /**
 To avoid the issue of Reflection on singleton object, enum can be used.
 Java ensures internally that the enum value is instantiated only once. Since java Enums are globally accessible, they can be used for singletons.
+The only drawback is that they do not support lazy initialization.
 **/
 package dev.designpattern.singleton;
 
